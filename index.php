8fa237d82310ce626c32db73fe523797d3cfed4b
MihailNovikow <mike10102000@gmail.com>
2024-01-16 18:12:54 +0300

    edited swagger, added /swagger route

--- index.php
+++ index.php
@@ -6,9 +6,52 @@
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <link rel="stylesheet" type="text/css" href="css/form.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="web/dist/swagger-ui.css" >
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <style>
+      html
+      {
+        box-sizing: border-box;
+        overflow: -moz-scrollbars-vertical;
+        overflow-y: scroll;
+      }
+      *,
+      *:before,
+      *:after
+      {
+        box-sizing: inherit;
+      }
+
+      body
+      {
+        margin:0;
+        background: #fafafa;
+      }
+    </style>
 </head>
 <body>
 	<div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js"></script>
+    <script>
+    window.onload = function() {
+      // Begin Swagger UI call region
+        console.log(window.location.pathname);
+      const ui = SwaggerUIBundle({
+        url: window.location.href + "/swagger.yaml",
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+          presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIStandalonePreset
+          ],
+          layout: "StandaloneLayout"
+      })
+      // End Swagger UI call region
+      window.ui = ui
+    }
+  </script>
     
 	<div class="phppot-container tile-container">
 
@@ -34,26 +77,6 @@
 		</form>
 	</div>
 	</div>
-	<script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js"></script>
-    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js"></script>
-    <script>
-    window.onload = function() {
-      // Begin Swagger UI call region
-        console.log(window.location.pathname);
-      const ui = SwaggerUIBundle({
-        url: window.location.href + "swagger.yaml",
-        dom_id: '#swagger-ui',
-        deepLinking: true,
-          presets: [
-              SwaggerUIBundle.presets.apis,
-              SwaggerUIStandalonePreset
-          ],
-          layout: "StandaloneLayout"
-      })
-      // End Swagger UI call region
-      window.ui = ui
-    }
-  </script>
 
 </body>
 </html>
